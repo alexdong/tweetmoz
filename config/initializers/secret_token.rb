f5c6c189f51e8632595b2f1c9b1d10723b25dc45
Alex Dong <me@alexdong.com>
2010-12-06 11:38:39 +0800

    Initial commit with Capfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetmoz::Application.config.secret_token = '22baccdb76c50cf79ff40205f428cc1016bbd9c436a684523db05f8d91acc8f6be7f3f21efb4073b3aea6b3501a24163a0c1ba43908b6f72496a187b2c223325'
